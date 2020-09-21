07694ca49e71394b67e905e587c05def49fce234
kaloyanker <36419542+kaloyanker@users.noreply.github.com>
2020-09-21 14:28:17 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -5,20 +5,87 @@ main () {
 }
 
 class MyApp extends StatelessWidget {
-  build(context) {
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xff3b3b3b),
 
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('ReaLife BattleRoyal'),
-            backgroundColor: Color.fromRGBO(0, 0, 0, 1),),
+      body:
 
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/facepalm.png'),
-          Text('Test Lorem Ipsum dolor sit amet')
-        ]))
+      Column(
 
-    ));
 
+        children: [
+          Center(
+                child: Container(
+
+                margin: EdgeInsets.only(top: 200), //tova trqbva da se napravi dinamichno
+
+                   child: CircleAvatar(
+                        radius: 150,
+                   // backgroundImage: AssetImage('images/maphome.PNG'),
+                         backgroundImage: NetworkImage('https://i.imgur.com/BatKcsf.png'),
+                        //backgroundImage: AssetImage('images/maphome.PNG')
+                    )
+          /*
+                  child: Image(
+                    width: 201,
+                    height: 141,
+                  image: AssetImage('images/maphome.PNG'),
+                  )*/
+                )
+          ),
+          Center(
+                child: Container(
+                  width: 250,
+                  height: 40,
+                  margin: EdgeInsets.only(top: 80), //tova trqbva da se napravi dinamichno
+                  padding: EdgeInsets.only(left: 0),
+                  /*
+                  decoration: BoxDecoration(
+                   border: Border.all(),
+                 ),*/
+
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FlatButton(
+                        textColor: Colors.white,
+                        disabledColor: Colors.grey,
+                        disabledTextColor: Colors.black,
+                        splashColor: Colors.white,
+                        padding: EdgeInsets.all(8.0),
+                        onPressed: () {
+                          /*...*/
+                        },
+                        child: Text(
+                          "Log-in",
+                        ),
+                      ),
+                      FlatButton(
+                        //color: Colors.blue,
+                        textColor: Colors.white,
+                        disabledColor: Colors.grey,
+                        disabledTextColor: Colors.black,
+                        padding: EdgeInsets.all(8.0),
+                        splashColor: Colors.white,
+                        onPressed: () {
+                          /*...*/
+                        },
+                        child: Text(
+                          "Sign-up",
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),//login
+        ],
+      )//LoginButtons
+      ),
+
+    );
   }
 }
