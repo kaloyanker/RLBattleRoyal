4a89fe9599086c211319694d181d6fb9c2ff1777
kaloyanker <36419542+kaloyanker@users.noreply.github.com>
2020-09-21 15:29:01 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -9,83 +9,99 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xff3b3b3b),
+      home:
 
-      body:
+        Scaffold(
 
-      Column(
+        body:
 
-
-        children: [
-          Center(
-                child: Container(
-
-                margin: EdgeInsets.only(top: 200), //tova trqbva da se napravi dinamichno
-
-                   child: CircleAvatar(
-                        radius: 150,
-                   // backgroundImage: AssetImage('images/maphome.PNG'),
-                         backgroundImage: NetworkImage('https://i.imgur.com/BatKcsf.png'),
-                        //backgroundImage: AssetImage('images/maphome.PNG')
-                    )
-          /*
-                  child: Image(
-                    width: 201,
-                    height: 141,
-                  image: AssetImage('images/maphome.PNG'),
-                  )*/
-                )
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage('https://i.imgur.com/1MxiyFC.png'),
+              fit: BoxFit.cover,
+            )
           ),
-          Center(
-                child: Container(
-                  width: 250,
-                  height: 40,
-                  margin: EdgeInsets.only(top: 80), //tova trqbva da se napravi dinamichno
-                  padding: EdgeInsets.only(left: 0),
-                  /*
-                  decoration: BoxDecoration(
-                   border: Border.all(),
-                 ),*/
 
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Center(
+            child: FractionallySizedBox(
+              widthFactor: 0.75,
+              child: Container(
+                color: Colors.black45,
+                child: Column(
                     children: [
-                      FlatButton(
-                        textColor: Colors.white,
-                        disabledColor: Colors.grey,
-                        disabledTextColor: Colors.black,
-                        splashColor: Colors.white,
-                        padding: EdgeInsets.all(8.0),
-                        onPressed: () {
-                          /*...*/
-                        },
-                        child: Text(
-                          "Log-in",
-                        ),
+                      Center(
+                            child: Container(
+
+                            margin: EdgeInsets.only(top: 200), //tova trqbva da se napravi dinamichno
+
+                               child: CircleAvatar(
+                                    radius: 150,
+                               // backgroundImage: AssetImage('images/maphome.PNG'),
+                                  //   backgroundImage: NetworkImage('https://i.imgur.com/BatKcsf.png'),
+                                     backgroundImage: NetworkImage('https://i.imgur.com/2ywEr1R.png'),
+                                )
+                      /*
+                              child: Image(
+                                width: 201,
+                                height: 141,
+                              image: AssetImage('images/maphome.PNG'),
+                              )*/
+                            )
                       ),
-                      FlatButton(
-                        //color: Colors.blue,
-                        textColor: Colors.white,
-                        disabledColor: Colors.grey,
-                        disabledTextColor: Colors.black,
-                        padding: EdgeInsets.all(8.0),
-                        splashColor: Colors.white,
-                        onPressed: () {
-                          /*...*/
-                        },
-                        child: Text(
-                          "Sign-up",
-                        ),
-                      )
+                      Center(
+                            child: Container(
+                              width: 250,
+                              height: 40,
+                              margin: EdgeInsets.only(top: 80), //tova trqbva da se napravi dinamichno
+                              padding: EdgeInsets.only(left: 0),
+                              /*
+                              decoration: BoxDecoration(
+                               border: Border.all(),
+                             ),*/
+
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  FlatButton(
+                                    textColor: Colors.white,
+                                    disabledColor: Colors.grey,
+                                    disabledTextColor: Colors.black,
+                                    splashColor: Colors.white,
+                                    padding: EdgeInsets.all(8.0),
+                                    onPressed: () {
+                                      /*...*/
+                                    },
+                                    child: Text(
+                                      "Log-in",
+                                    ),
+                                  ),
+                                  FlatButton(
+                                    //color: Colors.blue,
+                                    textColor: Colors.white,
+                                    disabledColor: Colors.grey,
+                                    disabledTextColor: Colors.black,
+                                    padding: EdgeInsets.all(8.0),
+                                    splashColor: Colors.white,
+                                    onPressed: () {
+                                      /*...*/
+                                    },
+                                    child: Text(
+                                      "Sign-up",
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),//login
                     ],
                   ),
-                ),
-              ),//login
-        ],
-      )//LoginButtons
-      ),
-
+              ),
+            ),
+          ),
+        ),
+        //LoginButtons
+        ),
     );
   }
 }
